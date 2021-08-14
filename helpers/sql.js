cddbc78f1c23e535254961e22c3c629e3f4b71f9
Jooah Seo <seo.jooah@gmail.com>
2021-08-13 21:43:36 -0400

    remove console.logs

--- helpers/sql.js
+++ helpers/sql.js
@@ -4,12 +4,10 @@ const { BadRequestError } = require("../expressError");
  *
  * Example:
  * dataToUpdate = { firstName: "John", isAdmin: true }
- * Returns {setCols: "first_name=$1,is_admin=$2", values: ["John", true]}
+ * Returns {setCols: '"first_name"=$1, "is_admin"=$2', values: ["John", true]}
  */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  console.log("This is data to update", dataToUpdate);
-  console.log("jsToSql: ", jsToSql);
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 

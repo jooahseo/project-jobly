4e039cf9ec92e58eff61bc4f60c0991208ab9fce
Jooah Seo <seo.jooah@gmail.com>
2021-08-14 19:51:12 -0400

    result can be filtered by user's query in findAll function

--- routes/companies.js
+++ routes/companies.js
@@ -52,7 +52,8 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
 
 router.get("/", async function (req, res, next) {
   try {
-    const companies = await Company.findAll();
+    const { name, minEmployees, maxEmployees } = req.query;
+    const companies = await Company.findAll(name, minEmployees, maxEmployees);
     return res.json({ companies });
   } catch (err) {
     return next(err);

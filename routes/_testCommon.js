fb3df249132c540d03a04f2c1541d87bec0cfe9b
Jooah Seo <seo.jooah@gmail.com>
2021-08-15 11:04:39 -0400

    added admin user for testing

--- routes/_testCommon.js
+++ routes/_testCommon.js
@@ -60,6 +60,14 @@ async function commonBeforeAll() {
     password: "password3",
     isAdmin: false,
   });
+  await User.register({
+    username: "admin",
+    firstName: "Admin",
+    lastName: "istration",
+    email: "admin@admin.com",
+    password: "password",
+    isAdmin: true,
+  });
 }
 
 async function commonBeforeEach() {
@@ -76,6 +84,7 @@ async function commonAfterAll() {
 
 
 const u1Token = createToken({ username: "u1", isAdmin: false });
+const adminToken = createToken({ username: "admin", isAdmin: true });
 
 
 module.exports = {
@@ -84,4 +93,5 @@ module.exports = {
   commonAfterEach,
   commonAfterAll,
   u1Token,
+  adminToken
 };

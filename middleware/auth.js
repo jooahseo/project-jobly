532d2d1244dfb1d1f9f14f000d76d924c66cf3ae
Jooah Seo <seo.jooah@gmail.com>
2021-08-15 11:01:49 -0400

    madded ensureAdmin middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,24 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/** Middleware to use when they must be logged in AND an admin.
+ *
+ * If not, raises Unauthorized.
+ */
+
+ function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user) throw new UnauthorizedError();
+    if (!res.locals.user.isAdmin) throw new UnauthorizedError("Admin Only");
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin
 };

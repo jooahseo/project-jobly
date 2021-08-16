341ccd0f7ad1452ce38e3eca09470e46c6da3bcb
Jooah Seo <seo.jooah@gmail.com>
2021-08-16 16:37:48 -0400

    jsToSql has default {} value

--- helpers/sql.js
+++ helpers/sql.js
@@ -7,7 +7,7 @@ const { BadRequestError } = require("../expressError");
  * Returns {setCols: '"first_name"=$1, "is_admin"=$2', values: ["John", true]}
  */
 
-function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+function sqlForPartialUpdate(dataToUpdate, jsToSql={}) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 

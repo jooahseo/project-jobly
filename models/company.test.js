4c629bbfb8753ca15a9e6dfd26740a82e6509509
Jooah Seo <seo.jooah@gmail.com>
2021-08-18 14:20:23 -0400

    updated a test for ccompany get class function

--- models/company.test.js
+++ models/company.test.js
@@ -111,6 +111,14 @@ describe("get", function () {
       description: "Desc1",
       numEmployees: 1,
       logoUrl: "http://c1.img",
+      jobs: [
+        {
+          id: 1,
+          title: "j1",
+          salary: 1,
+          equity: "0.001",
+        }
+      ]
     });
   });
 

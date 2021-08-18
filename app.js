231900856cfeb2b1b41d4fc10b608350bff5e33d
Jooah Seo <seo.jooah@gmail.com>
2021-08-17 21:08:39 -0400

    added a job route

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const companiesRoutes = require("./routes/companies");
 const usersRoutes = require("./routes/users");
+const jobRoutes = require("./routes/jobs");
 
 const morgan = require("morgan");
 
@@ -24,6 +25,7 @@ app.use(authenticateJWT);
 app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
+app.use("/jobs", jobRoutes);
 
 
 /** Handle 404 errors -- this matches everything */
